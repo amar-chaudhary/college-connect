709b41d43fde933458b18085a181db5bd7cddde0
Shadovw <adityabss47@gmail.com>
2023-05-05 23:53:33 +0530

    removed chat__button

--- public/js/room.js
+++ public/js/room.js
@@ -5,7 +5,6 @@ const memberContainer = document.getElementById('members__container');
 const memberButton = document.getElementById('members__button');
 
 const chatContainer = document.getElementById('messages__container');
-const chatButton = document.getElementById('chat__button');
 
 let activeMemberContainer = false;
 
@@ -20,16 +19,6 @@ memberButton.addEventListener('click', () => {
 });
 
 let activeChatContainer = false;
-
-chatButton.addEventListener('click', () => {
-  if (activeChatContainer) {
-    chatContainer.style.display = 'none';
-  } else {
-    chatContainer.style.display = 'block';
-  }
-
-  activeChatContainer = !activeChatContainer;
-});
 
 let displayFrame = document.getElementById('stream__box')
 let videoFrames = document.getElementsByClassName('video__container')
